6c6729a5910a359a570d28e486d595441802a2b2
DESKTOP-6A8O42R\Crosis <www30506@gmail.com>
2017-10-21 19:46:04 +0800

    【新增】 多語系插件 【新增】 頁面管理器插件 【新增】 ProjectSetting

---
+++ Billing/Assets/CPlugins/PageManerger/PageManerger_Example/Test_Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Controller : MonoBehaviour {
+	
+	IEnumerator Start () {
+		PageManerger.CloseAllPage ();
+		yield return null;
+		PageManerger.ChangePage (PageType.Test_MainPage);
+	}
+	
+	void Update () {
+		
+	}
+}
